7ac147ee0c89bf15fda1032f7bf64ed0ba76f916
george murphy <georgeolivermurphy@hotmail.com>
2021-06-07 13:19:03 +0100

    search function

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const BASE_URL = 'https://api.themoviedb.org/3'
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'
 +API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
 const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
 getMovies(API_URL);
 
@@ -57,3 +60,16 @@ function getColor(vote) {
 
 }
 
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if(searchTerm) {
+    getMovies(searchURL+'&query='+searchTerm)
+  }else{
+    getMovies(API_URL);
+  }
+
+})
