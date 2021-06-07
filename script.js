aa6a6c1ea66baee1949bebab3a87018eed728323
george murphy <georgeolivermurphy@hotmail.com>
2021-06-07 13:02:07 +0100

    get element by id

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'
 +API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
+const main = document.getElementById('main');
+
 getMovies(API_URL);
 
 function getMovies(url) {
@@ -19,24 +21,28 @@ function getMovies(url) {
 }
 
 function showMovies(data) {
+  main.innerHTML = '';
 
   data.forEach(movie => {
       const {title, poster_path, vote_average, overview} = movie
       const movieEl = document.createElement('div');
       movieEl.classList.add('movie');
-      movie.El.innerHTML = `
-      <img src="${IMG_URL+poster_path}" alt="${title}">
+      movieEl.innerHTML = `
+        <img src="${IMG_URL+poster_path}" alt="${title}">
 
-        <div class ="movie-info">
-            <h3>${title}</h3>
-            <span class="${getColor(vote_average)}">${vote_average}</span>
-        </div>
+          <div class ="movie-info">
+              <h3>${title}</h3>
+              <span class="${getColor(vote_average)}">${vote_average}</span>
+          </div>
 
-        <div class="overview">
-            <h3>Overview</h3>
-            ${overview}
-        </div>
+          <div class="overview">
+              <h3>Overview</h3>
+              ${overview}
+          </div>
       `
+
+
+      main.appendChild(movieEl);
   })
 }
 
